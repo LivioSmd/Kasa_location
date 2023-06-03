2f3751ca57bb124caa9740194c836f9f664aaf05
Livio Simard <liviosimard@yahoo.fr>
2023-06-03 19:32:02 +0200

    creation of page files and folders

---
+++ src/pages/accomodation/Accomodation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./accomodation.scss";
+
+function Accomodation() {
+    return <div>accomodation</div>
+}
+
+export default Accomodation; 
---
+++ src/pages/error/Error.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./error.scss";
+
+function Error() {
+    return <div>Error 404</div>
+}
+
+export default Error; 
