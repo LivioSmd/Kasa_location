035462da810abcda0e49ee17688713efb8261189
Livio Simard <liviosimard@yahoo.fr>
2023-06-09 11:23:42 +0200

    add logement file logic

--- src/pages/accomodation/Accomodation.jsx
+++ src/pages/accomodation/Accomodation.jsx
@@ -1,9 +1,31 @@
-import React from "react";
 import "../../Styles/Accomodation.scss";
+import logements from "../../datas/Logements/logements.json";
+import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
-
-function Accomodation() {
-    return <div>liste des hebergements</div>
+function Affiche({ logement }) {
+  return (
+    <div>
+      <h4>{logement.title}</h4>
+      <img src={logement.cover} alt="logement disponible" />
+    </div>
+  );
 }
 
-export default Accomodation; 
+function Accomodation() {
+    const { id } = useParams();
+    const [logement, setLogement] = useState(null);
+
+    useEffect(() => {
+        const logementFound = logements.find((logement) => logement.id === id);
+        setLogement(logementFound);
+    }, [id]);
+
+    return (
+        <div>
+            {logement ? <Affiche logement={logement} /> : <p>Logement introuvable</p>}
+        </div>
+  );
+}
+
+export default Accomodation;
