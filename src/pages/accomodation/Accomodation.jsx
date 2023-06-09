b882c1e19ee5c359981eab8e57dafcf8d4e43983
Livio Simard <liviosimard@yahoo.fr>
2023-06-09 18:26:51 +0200

    import all json in accomodation and temporary css changes

--- src/pages/accomodation/Accomodation.jsx
+++ src/pages/accomodation/Accomodation.jsx
@@ -4,12 +4,25 @@ import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 
 function Affiche({ logement }) {
-  return (
-    <div>
-      <h4>{logement.title}</h4>
-      <img src={logement.cover} alt="logement disponible" />
-    </div>
-  );
+  return <>
+        <div>
+            {logement.pictures.map((img, index) => {
+                return <img src={img} key={index} alt="" />
+            })}
+        </div>
+        <div>
+            <img src={logement.cover} alt="logement disponible" />
+            <img src={logement.pictures} alt="" />
+            <h4>{logement.title}</h4>
+            <h4>{logement.host.name}</h4>
+            <img src={logement.host.picture} alt="" />
+            <h4>{logement.location}</h4>
+            <p>{logement.tags}</p>
+            <p>{logement.rating}</p>
+            <h2>{logement.description}</h2>
+            <p>{logement.equipments}</p>
+        </div>
+    </>
 }
 
 function Accomodation() {
