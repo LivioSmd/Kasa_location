26d2fda09883ed86402673b8afc98ca63fc3cf9f
Livio Simard <liviosimard@yahoo.fr>
2023-06-16 11:20:48 +0200

    fix collapse icon rotate and size

--- src/components/Collapse/CollapseForEquipements.jsx
+++ src/components/Collapse/CollapseForEquipements.jsx
@@ -3,15 +3,19 @@ import './Collapse.css';
 
 function CollapseForEquipements ({name, children}) {
     const [isCollapseOpen, setIsCollapseOpen] = useState(false);
+    const [isDown, setIsDown] = useState(false);
+
+    const handleClick = () => {
+        setIsDown(!isDown);
+    };
 
     return <div className='collapse'>
-        <div
-            onClick={() => setIsCollapseOpen(isCollapseOpen => !isCollapseOpen)}
-            className='collapseHeader'
-        >
+        <div className='collapseHeader'>
             <h4 className='title-collapse'>{name}</h4>
-            <div className='div-icon'>
-                <i className={isCollapseOpen ? 'fas fa-chevron-down' : 'fas fa-chevron-up'}></i>
+            <div onClick={() => setIsCollapseOpen(isCollapseOpen => !isCollapseOpen)}>
+                <div className='div-icon'>
+                    <div className={`fas fa-chevron-up rotate ${isDown ? 'down' : ''}`} onClick={handleClick}></div>
+                </div>
             </div>
         </div>
         {isCollapseOpen && <div className='collapseBody'>
